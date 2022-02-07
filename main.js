efaf43d8243d07ee4ac3aec63141fb98c0cf9568
Klayton Kowalski <klaytonkowalski@gmail.com>
2022-02-07 16:16:08 -0500

    WIP 'lighting.js' example.

--- main.js
+++ main.js
@@ -4,9 +4,10 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/three', express.static(path.join(__dirname, 'node_modules', 'three', 'build')));
+app.use('/build', express.static(path.join(__dirname, 'node_modules', 'three', 'build')));
+app.use('/jsm', express.static(path.join(__dirname, 'node_modules', 'three', 'examples', 'jsm')));
 
 app.listen(4106, () =>
 {
-    console.log('Visit http://localhost:4106/ to use example-threejs.');
+    console.log('Visit http://localhost:4106/.');
 });
---
+++ public/examples/lighting.js
@@ -0,0 +1,79 @@
+import * as THREE from '/build/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls.js'
+
+const elCanvas = document.getElementById('canvas')
+
+const renderer = new THREE.WebGLRenderer({ canvas: elCanvas })
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(75, 2, 0.1, 1000)
+camera.position.set(0, 10, 20)
+
+const controls = new OrbitControls(camera, elCanvas)
+controls.target.set(0, 5, 0)
+controls.update()
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+const meshes = []
+
+///////////////////////////////////////////////////////////////////////////////
+// TEXTURES, LOADING
+///////////////////////////////////////////////////////////////////////////////
+
+const loader = new THREE.TextureLoader();
+const texture = loader.load('../assets/texture-7.png')
+texture.wrapS = THREE.RepeatWrapping
+texture.wrapT = THREE.RepeatWrapping
+texture.magFilter = THREE.NearestFilter
+texture.repeat.set(20, 20)
+
+const planeGeometry = new THREE.PlaneGeometry(40, 40)
+const planeMaterial = new THREE.MeshPhongMaterial({ map: texture, side: THREE.DoubleSide })
+const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial)
+planeMesh.rotation.x = Math.PI * -0.5
+scene.add(planeMesh)
+
+const cubeGeometry = new THREE.BoxGeometry(4, 4, 4)
+const cubeMaterial = new THREE.MeshPhongMaterial({ color: '#8AC' })
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial)
+cubeMesh.position.set(6, 3, 0)
+scene.add(cubeMesh)
+meshes.add(cubeMesh)
+
+const sphereGeometry = new THREE.SphereGeometry(4, 8, 8)
+const sphereMaterial = new THREE.SphereMaterial({ color: '#CA8' })
+const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial)
+sphereMesh.postition.set(-6, 3, 0)
+scene.add(sphereMesh)
+meshes.add(sphereMesh)
+
+const ambientLight = new THREE.AmbientLight('#FFF', 1)
+scene.add(ambientLight)
+
+///////////////////////////////////////////////////////////////////////////////
+
+function checkResize()
+{
+    return elCanvas.width != elCanvas.clientWidth || elCanvas.height != elCanvas.clientHeight
+}
+
+function render(dt)
+{
+    if (checkResize())
+    {
+        renderer.setSize(elCanvas.clientWidth, elCanvas.clientHeight, false)
+        camera.aspect = elCanvas.clientWidth / elCanvas.clientHeight
+        camera.updateProjectionMatrix();
+    }
+    for (const mesh of meshes)
+    {
+        mesh.rotation.x = dt * 0.001
+        mesh.rotation.y = dt * 0.001
+    }
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
+}
+
+requestAnimationFrame(render)
